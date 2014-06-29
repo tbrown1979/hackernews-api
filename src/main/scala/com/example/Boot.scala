1746649ca05956b90a593a76fbbf9ed26557e36d
Taylor Brown <taylor.brown@banno.com>
2014-06-29 14:07:55 -0500

    update port for heroku compatibility

--- src/main/scala/com/example/Boot.scala
+++ src/main/scala/com/example/Boot.scala
@@ -3,15 +3,20 @@ package com.example
 import akka.actor.{ActorSystem, Props}
 import akka.io.IO
 import spray.can.Http
+import util.Properties
+
 
 object Boot extends App {
 
   // we need an ActorSystem to host our application in
   implicit val system = ActorSystem("on-spray-can")
 
+  val port = Properties.envOrElse("PORT", "8080").toInt // for Heroku compatibility
+
+
   // create and start our service actor
   val service = system.actorOf(Props[MyServiceActor], "demo-service")
 
   // start a new HTTP server on port 8080 with our service actor as the handler
-  IO(Http) ! Http.Bind(service, interface = "localhost", port = 8080)
+  IO(Http) ! Http.Bind(service, "0.0.0.0", port)
 }
